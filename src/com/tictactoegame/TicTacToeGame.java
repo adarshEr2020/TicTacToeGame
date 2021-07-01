316f9478c5c1bb649caaa90b913b49e3e9341c9f
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-01 10:12:45 +0530

    Added: creating game board

--- src/com/tictactoegame/TicTacToeGame.java
+++ src/com/tictactoegame/TicTacToeGame.java
@@ -2,7 +2,21 @@ package com.tictactoegame;
 
 public class TicTacToeGame {
 	
+	// create play board
+	public char[] playBoard(){
+		char []board = new char[10];
+		for(int i = 0; i < board.length; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+	
+
 	public static void main(String[] args) {
 		System.out.println("welcome to tic tac toe game : ");
+		TicTacToeGame tictactoe = new TicTacToeGame();
+		char []board = tictactoe.playBoard();
+		System.out.println(tictactoe.playBoard());
+	
 	}
 }
