a2aa05bda8ed0190394489ee2ab24b524d3f7329
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-01 14:21:46 +0530

    Added: choose letter X or O computer or user determine

--- src/com/tictactoegame/TicTacToeGame.java
+++ src/com/tictactoegame/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.tictactoegame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	
 	// create play board
@@ -11,12 +13,30 @@ public class TicTacToeGame {
 		return board;
 	}
 	
-
+	// method for choose user letter for game
+	public char chooseLetter() {
+		char letter;
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Choose the letter O or X :");
+		letter = userInput.next().toUpperCase().charAt(0);
+		return letter;
+	}
+	
+	// main method 
 	public static void main(String[] args) {
 		System.out.println("welcome to tic tac toe game : ");
 		TicTacToeGame tictactoe = new TicTacToeGame();
 		char []board = tictactoe.playBoard();
-		System.out.println(tictactoe.playBoard());
-	
+		char userLetter = tictactoe.chooseLetter();
+		char computerLetter;
+		
+		if(userLetter == 'X') {
+			computerLetter = 'O';
+		}else {
+			computerLetter = 'X';
+		}
+	System.out.println("Computer letter is : " +computerLetter +" "+"And User letter is : " +userLetter);
 	}
+
+	
 }
